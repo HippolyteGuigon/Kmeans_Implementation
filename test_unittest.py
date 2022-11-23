8c8925dee5fde6970d7cc2e1fd2499d9b98329af
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2022-11-23 16:07:30 +0100

    final_push

--- test_unittest.py
+++ test_unittest.py
@@ -7,12 +7,8 @@ from src.confs.confs import load_conf
 test = Data_Generator()
 model_test = Model()
 
-with open("configs/data_params.yml", "r") as ymlfile:
-    configs = yaml.safe_load(ymlfile)
-
-with open("configs/model_params.yml", "r") as ymlfile:
-    configs_model = yaml.safe_load(ymlfile)
-
+configs=load_conf("configs/data_params.yml")
+configs_model=load_conf("configs/model_params.yml")
 
 class Test(unittest.TestCase):
     """
@@ -20,17 +16,13 @@ class Test(unittest.TestCase):
     and check everything commited makes sense
     """
 
-    def __init__(self,path_config="configs/data_params.yml",path_model="configs/model_params.yml"):
-        self.configs=load_conf(path_config)
-        self.configs_model=load_conf(path_model)
-
     def test_generated_data(self):
         test_data = test.generate_data()
 
-        n_rows = self.configs["number_of_individuals"]
-        n_columns = self.configs["number_dimension"]
-        lim_min = self.configs["limit_min"]
-        lim_max = self.configs["limit_max"]
+        n_rows = configs["number_of_individuals"]
+        n_columns = configs["number_dimension"]
+        lim_min = configs["limit_min"]
+        lim_max = configs["limit_max"]
 
         self.assertEqual(test_data.shape[0], n_rows)
         self.assertEqual(test_data.shape[1], n_columns)
@@ -39,8 +31,8 @@ class Test(unittest.TestCase):
 
     def test_initial_centroids(self):
         test_centroid = model_test.generate_initial_K()
-        self.assertEqual(test_centroid.shape[0], self.configs_model["K"])
-        self.assertEqual(test_centroid.shape[1], self.configs_model["number_dimension"])
+        self.assertEqual(test_centroid.shape[0], configs_model["K"])
+        self.assertEqual(test_centroid.shape[1], configs["number_dimension"])
 
 
 if __name__ == "__main__":
