4ef5ba96aa0afa66ed2e13d3d13c274c709d27ce
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2022-11-24 01:53:29 +0100

    pushing_argparse

--- main.py
+++ main.py
@@ -15,13 +15,19 @@ formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
 
 main()
 
+parser=argparse.ArgumentParser()
+parser.add_argument("Name",
+help="The name entered by the user to easily find your own iteration in the logs",
+type=str)
+args=parser.parse_args()
+
 def model_launch():
     model = Model()
-    logger.info("Model Charged")
+    logger.info(f"Model Charged {args.Name}")
     model.generate_initial_K()
-    logger.info("Initial centroïds initialized")
+    logger.info(f"Initial centroïds initialized {args.Name}")
     model.compute_distances()
-    logger.info("First iteration done")
+    logger.info(f"First iteration done {args.Name}")
 
 if __name__=="__main__":
     model_launch()
