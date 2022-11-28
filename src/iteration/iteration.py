e9b8667799ac520b938d1df8abc62751a87b34f1
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2022-11-28 03:19:46 +0100

    pushing_first_centroids

--- src/iteration/iteration.py
+++ src/iteration/iteration.py
@@ -1,14 +1,18 @@
 import numpy as np
-import sys 
+import sys
 import os
+
 sys.path.insert(0, os.path.join(os.getcwd(), "src/confs"))
 from confs import load_conf
+from scipy import spatial
+
 
 class Generate_Region:
-
-    def __init__(self,
+    def __init__(
+        self,
         path_config_model="configs/model_params.yml",
-        path_config_file="configs/data_params.yml"):
+        path_config_file="configs/data_params.yml",
+    ):
 
         self.configs = load_conf(path_config_file)
         self.configs_model = load_conf(path_config_model)
@@ -18,7 +22,7 @@ class Generate_Region:
         Generates the data randomly according to the configs
         file
         """
-        n_rows = 1000*self.configs["number_of_individuals"]
+        n_rows = 1000 * self.configs["number_of_individuals"]
         n_columns = self.configs["number_dimension"]
         lim_min = self.configs["limit_min"]
         lim_max = self.configs["limit_max"]
@@ -26,3 +30,24 @@ class Generate_Region:
             low=lim_min, high=lim_max, size=(n_rows, n_columns)
         )
         return data_generated
+
+    def compute_centroid(self, data_points):
+        """
+        The goal of this function is to determine  each cluster zone of
+        influence that will then be used to compute the centroids. Points are generated
+        randomly and attributed to nearest cluster's appartenance points.
+        Regions emerging from this operation are then used to compute centroid
+
+        Args:
+        data_points: np.array(): The original points with attributed clusters at a cetrain step
+
+        Return:
+        np.array() with the centroids of each clusters"""
+        data_region = self.initiate_region_points()
+        clusters = data_points[:, -1]
+        data_points = data_points[:, :-1]
+        closest_points = spatial.KDTree(data_points).query(data_region)[1]
+        attributed_regions = clusters[closest_points]
+        data_region = np.column_stack((data_region, attributed_regions))
+        unique_centroids = np.unique(clusters)
+        return data_region
--- src/model/model.py
+++ src/model/model.py
@@ -2,7 +2,6 @@ import numpy as np
 import sys
 import os
 from sklearn.metrics.pairwise import pairwise_distances
-from scipy import spatial
 from src.confs.confs import load_conf
 
 sys.path.insert(0, os.path.join(os.getcwd(), "src/confs"))
@@ -12,7 +11,8 @@ sys.path.insert(0, os.path.join(os.getcwd(), "src/iteration"))
 from data_generator import Data_Generator
 from iteration import Generate_Region
 
-class Model(Data_Generator,Generate_Region):
+
+class Model(Data_Generator, Generate_Region):
     """
     The goal of this class is to implement the KMeans
     algorithm step by step
@@ -28,7 +28,8 @@ class Model(Data_Generator,Generate_Region):
         self.configs_model = load_conf(path_config_model)
         self.K = self.configs_model["K"]
         self.data = super().generate_data()
-        self.data_region=super().initiate_region_points()
+        self.data_region = super().initiate_region_points()
+        self.generate_region = Generate_Region()
 
     def generate_initial_K(self):
         """
@@ -47,31 +48,23 @@ class Model(Data_Generator,Generate_Region):
     def first_attribution(self):
         distances = pairwise_distances(self.data, self.initial_coordinates)
         cluster_belonging = np.argmin(distances, axis=1)
-        full_data=np.column_stack((self.data,cluster_belonging))
-        np.save(self.configs_model["path_save"],full_data)
+        full_data = np.column_stack((self.data, cluster_belonging))
+        np.save(self.configs_model["path_save"], full_data)
         return full_data
 
-    def compute_centroid(self,data_points):
-        """
-        The goal of this function is to determine  each cluster zone of 
-        influence that will then be used to compute the centroids. Points are generated 
-        randomly and attributed to nearest cluster's appartenance points.
-        Regions emerging from this operation are then used to compute centroid
-        
-        Args:
-        data_points: np.array(): The original points with attributed clusters at a cetrain step
-        
-        Return:
-        np.array() with the centroids of each clusters"""
+    def cluster_attribution(self, centroids):
+        distances = pairwise_distances(self.data, centroids)
+        cluster_belonging = np.argmin(distances, axis=1)
+        full_data = np.column_stack((self.data, cluster_belonging))
+        np.save(self.configs_model["path_save"], full_data)
+        return full_data
 
-        full_data=self.first_attribution()
-        data_points=full_data[:,:-1]
-        attributed_regions=spatial.KDTree(data_points).query(self.data_region)[1]
-        print(attributed_regions)
-
+    def launch_iteration(self):
+        self.current_repartition = self.first_attribution()
+        self.generate_region.compute_centroid(self.current_repartition)
 
 
 a = Model()
 a.generate_initial_K()
 a.first_attribution()
-a.compute_centroid(a.first_attribution())
+a.launch_iteration()
